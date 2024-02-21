f0a458ce9e82b4caa0a79625a978aa09a6499d8a
Viktor Golev <vwpiligrim@gmail.com>
2024-02-21 14:53:55 +0300

    Combined all parts together

--- filter_strings_by_len.cpp
+++ filter_strings_by_len.cpp
@@ -4,17 +4,20 @@
 #include <algorithm>
 #include <cstring>
 // Функция для получения ввода от пользователя
-std::string getInput() {
+std::string getInput()
+{
     std::cout << "Введите элементы массива через запятую и пробел:\n";
     std::string line;
     std::getline(std::cin, line);
     return line;
 }
 // Функция для разделения строки на массив строк
-void splitString(const std::string& inputStr, std::vector<std::string>& result) {
+void splitString(const std::string &inputStr, std::vector<std::string> &result)
+{
     std::istringstream iss(inputStr);
     std::string token;
-    while (std::getline(iss, token, ',')) {
+    while (std::getline(iss, token, ','))
+    {
         // Удаляем пробелы в начале и конце строки
         token.erase(0, token.find_first_not_of(' '));
         token.erase(token.find_last_not_of(' ') + 1);
@@ -23,25 +26,44 @@ void splitString(const std::string& inputStr, std::vector<std::string>& result) 
 }
 
 // Функция для фильтрации строк по длине и формирования нового массива
-void filterStringsByLength(const std::vector<std::string>& input, std::vector<std::string>& result) {
-    for (const auto& str : input) {
-        if (str.length() <= 3) {
+void filterStringsByLength(const std::vector<std::string> &input, std::vector<std::string> &result)
+{
+    for (const auto &str : input)
+    {
+        if (str.length() <= 3)
+        {
             result.push_back(str);
         }
     }
 }
 
 // Функция для вывода результата на экран
-void printResult(const std::vector<std::string>& result) {
+void printResult(const std::vector<std::string> &result)
+{
     std::cout << "\nРезультат:\n";
     bool firstElementPrinted = false;
-    for (const auto& s : result) {
-        if (!firstElementPrinted) {
+    for (const auto &s : result)
+    {
+        if (!firstElementPrinted)
+        {
             firstElementPrinted = true;
-        } else {
+        }
+        else
+        {
             std::cout << ',';
         }
         std::cout << s;
     }
     std::cout << '\n';
+}
+
+int main()
+{
+    std::string inputStr = getInput();
+    std::vector<std::string> result;
+    splitString(inputStr, result);
+    std::vector<std::string> filteredResult;
+    filterStringsByLength(result, filteredResult);
+    printResult(filteredResult);
+    return 0;
 }
